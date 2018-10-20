02b2304c315c6da7e1e79d102e140fe98f3fe888
gsmartagence <gilles.gagner@gmail.com>
2018-10-20 17:55:06 +0200

    random collection

--- sw.js
+++ sw.js
@@ -4,10 +4,6 @@ importScripts('/_nuxt/workbox.42554690.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/000fe8b166795989660d.js",
-    "revision": "ab759687d7874b2b09ad132c94783e8f"
-  },
-  {
     "url": "/_nuxt/178b5d588a023952ba7d.js",
     "revision": "023585b4172b37feb51f74486406bcd5"
   },
@@ -20,12 +16,12 @@ workbox.precaching.precacheAndRoute([
     "revision": "67dcc3df2ec117de6152bb83a77212e7"
   },
   {
-    "url": "/_nuxt/53d73d6d48b783152041.js",
-    "revision": "9fd70bf9514b5f9f6ed96d903386ec74"
-  },
-  {
     "url": "/_nuxt/6e0e989a36e36c9205b9.js",
     "revision": "823f618ef9606a0d7f658ddf06560c5c"
+  },
+  {
+    "url": "/_nuxt/8453901914d88ea4acbc.js",
+    "revision": "51973abe6a943e570d8e2a19bbdb4f5c"
   },
   {
     "url": "/_nuxt/a50881b1445b4b5248b2.js",
@@ -38,6 +34,10 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "/_nuxt/e8a24e00eb63bdc57121.js",
     "revision": "95b157f1feaa21e00c8258a2ae628847"
+  },
+  {
+    "url": "/_nuxt/f2267931aaad2ff01c4b.js",
+    "revision": "5f9ac188b039e2e3008d7b081b31a1e3"
   },
   {
     "url": "/_nuxt/fdf9bf9053ffb872c53d.js",

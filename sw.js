e8d517abcba04a56e7ed9a7f6c6a40a5c7d74774
gsmartagence <gilles@smartagence.com>
2018-11-17 18:56:58 +0100

    weightwatcher session

--- sw.js
+++ sw.js
@@ -14,6 +14,10 @@ workbox.precaching.precacheAndRoute([
     "revision": "ead83429e79801c58db0b3cdfa88bb17"
   },
   {
+    "url": "/_nuxt/2d917f2f679af380f9ee.js",
+    "revision": "66c6d064604c8cbfd086b3a14a2a9100"
+  },
+  {
     "url": "/_nuxt/301a0bc0266195212c34.js",
     "revision": "7411951a03c8eb61bb8fab4c0e2a0634"
   },
@@ -24,10 +28,6 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "/_nuxt/8089b8a65a554a89bf8d.js",
     "revision": "759453931e440fe3dfd796589d0203c8"
-  },
-  {
-    "url": "/_nuxt/9dc13718df95b9066807.js",
-    "revision": "cdd7e54004e0337ec0a87bc7a68f6ee8"
   },
   {
     "url": "/_nuxt/a8451c35b63f4e11ca08.js",

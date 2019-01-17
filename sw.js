7b8df09b1aba5d4978dd5f986d442cc617935d2a
gsmartagence <gilles@smartagence.com>
2019-01-17 14:38:07 +0100

    bfsctn

--- sw.js
+++ sw.js
@@ -2,6 +2,10 @@ importScripts('/_nuxt/workbox.42554690.js')
 
 workbox.precaching.precacheAndRoute([
   {
+    "url": "/_nuxt/11344c67c1cb2fe35ecd.js",
+    "revision": "4f57fac56f8d4b944d82b0d1af193610"
+  },
+  {
     "url": "/_nuxt/3551c0fa3f482154bf99.js",
     "revision": "cd15811a0fc9159bb0124af973de47f3"
   },
@@ -10,36 +14,32 @@ workbox.precaching.precacheAndRoute([
     "revision": "3c16e8b8ad1cf251bd3c0af36d980a61"
   },
   {
-    "url": "/_nuxt/56818d97f08e2e56be0e.js",
-    "revision": "e3389a93a83ead800095f9737c675358"
+    "url": "/_nuxt/605a902c892a5711acf0.js",
+    "revision": "ff698a151f03bc89cad084ed03ddcefb"
   },
   {
-    "url": "/_nuxt/6eec009f8c33f8b4c74c.js",
-    "revision": "a53f9474cbd5a38c00ca593a72ab7ab6"
+    "url": "/_nuxt/6da143c34b4392096271.js",
+    "revision": "b18a4f59682e8eb1c64d6fbd5166c3aa"
   },
   {
-    "url": "/_nuxt/79d5cde1f5db5de6c8de.js",
-    "revision": "085557516464b32115602cfbf8fe5fd6"
+    "url": "/_nuxt/8e095c559a9a292cc4bc.js",
+    "revision": "ce237778da88ad709e156f4ac54acfa9"
   },
   {
-    "url": "/_nuxt/b82460563603bf8147a4.js",
-    "revision": "556f06a3e21e9914b686c8541bd3b573"
+    "url": "/_nuxt/97412e2e4d128c128796.js",
+    "revision": "92d2ce2c8e093559cba4482458f7a7d6"
   },
   {
-    "url": "/_nuxt/c5e3f58d7d3b2684315a.js",
-    "revision": "3c864c44a928c3e900a8dce4c57c8aa2"
+    "url": "/_nuxt/c252f4c969c6cf2338eb.js",
+    "revision": "faa512cf0fd1d03f4d56e16c5af24261"
   },
   {
-    "url": "/_nuxt/c65b550571c8950f0eb3.js",
-    "revision": "74441897ede9a2d7c7f35516e9a2553f"
+    "url": "/_nuxt/cf05dc1dee0333671e21.js",
+    "revision": "cd97392df9d3bc05aff712cb49825dc1"
   },
   {
     "url": "/_nuxt/ea0841936d51aaeaf728.js",
     "revision": "0890f94814433c1ebd7159b896c5fd9d"
-  },
-  {
-    "url": "/_nuxt/fd2b4d67d7acf00e6d37.js",
-    "revision": "bbd72ad2559b6890a4cb2c8f90ab4e32"
   }
 ], {
   "cacheId": "settanta",
